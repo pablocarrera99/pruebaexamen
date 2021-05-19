d09124e32110652e5e1ae64431cc2955b25ce3b0
pablocarrera <pablocarrera99@gmail.com>
2021-05-19 14:05:36 +0200

    prueba 2

---
+++ pruebaexamen/src/pruebaexamen/Casa.java
@@ -0,0 +1,18 @@
+package pruebaexamen;
+
+
+	public class Casa {
+		private float superficie;
+		private String calle;
+		private int codigoPostal;
+
+		/**
+		* Un equipo de ladrones deja la casa limpia
+		* y roba hasta el cobre de las paredes
+		**/ 
+		public void limpiarLaCasa() {
+		String mensajito ="Te hemos limpiado la casa, estás arruinado";
+		System.out.println(mensajito);
+		}
+		}
+
